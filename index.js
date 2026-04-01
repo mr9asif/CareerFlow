bb35a938316f758f76fb463efffcc6c7f094ed09
mr9 asif <asifalibd002@gmail.com>
2026-04-01 20:22:13 +0600

    connect db

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db")
 
 const app = express();
+
+// connect DB
+connectDB();
+
 
 app.get("/", (req, res) => {
   res.send("Server running 🚀");
