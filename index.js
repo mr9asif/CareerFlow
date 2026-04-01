e1c944786074fa6d37c91f36b16c4cc4b63cb891
mr9 asif <asifalibd002@gmail.com>
2026-04-01 20:02:32 +0600

    added packege and server running

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server running 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
